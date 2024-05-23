ae6f74bfbd5c5454bcc9e9390779aa84358f792f
RamblerGoodness <thomasandersaen.117@gmail.com>
2024-05-22 19:45:16 -0600

    added transparancy

--- Display/Character.py
+++ Display/Character.py
@@ -1,7 +1,8 @@
 # Example file showing a basic pygame "game loop"
 import pygame
-import threading
-import time
+import win32api
+import win32con
+import win32gui
 import os
 
 
@@ -21,16 +22,33 @@ class Character():
         pygame.init()
         clock = pygame.time.Clock()
         screen = pygame.display.set_mode((self.width, self.height), pygame.NOFRAME, pygame.SCALED)
-        # screen.
         pygame.display.set_caption(self.character_name)
-        screen.fill('green')
-        running = True
         
+        #Key out color.
+        green = (0, 255, 0)
+        
+        
+        # Create layered window
+        hwnd = pygame.display.get_wm_info()["window"]
+        win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE,
+                       win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE) | win32con.WS_EX_LAYERED)
+        
+        # Set window transparency color
+        win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(*green), 0, win32con.LWA_COLORKEY)
+        
+        # Set window to key out color
+        screen.fill(green)
+        
+        
+        # Start game loop
+        running = True
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
 
+            pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(30, 30, 60, 60))
+            win32gui.SetWindowPos(hwnd, win32con.HWND_TOPMOST, 0, 0, 0, 0, win32con.SWP_NOMOVE | win32con.SWP_NOSIZE)
             clock.tick(60)
             pygame.display.flip()
 
