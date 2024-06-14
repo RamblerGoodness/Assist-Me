7c0ef3b9d852d22e14cd24fd5c720b2ac29f5618
RamblerGoodness <thomasandersaen.117@gmail.com>
2024-06-14 13:09:09 -0600

    feat: Add Talky.py script for text-to-speech conversion

--- Talky.py
+++ Talky.py
@@ -6,7 +6,7 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 
 # Print device
 print( "device: ", device.upper, "\n" )
-
+print("\n")
 
 # List available 🐸TTS models
 print("models:")
@@ -19,19 +19,14 @@ print("\n")
 tts = TTS("tts_models/en/jenny/jenny").to(device)
 
 
-def tts_to_file(text:str, filename="output.wav", autoplay=False):
+def tts_to_file(text : str, filename="output.wav", autoplay=False):
     """Convert text to speech and save to file"""
-    
+
     # Synthesize text
-    wav = tts.tts(text)
-    
-    # Save to file
-    tts.save_wav(wav, filename)
+    tts.tts_to_file(text=text, file_path=filename)
     if autoplay:
         os.system(f"start {filename}")
 
-
-
 if __name__ == "__main__":
-    tts.tts_to_file(text="What are you doing in bed, shouldn't you be getting up and taking a shower?")
+    tts_to_file(text="What are you doing in bed, shouldn't you be getting up and taking a shower?")
     os.system("start output.wav")
